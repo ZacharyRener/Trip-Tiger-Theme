032bcc5bb6ae213ca3f69e0981ecc83cfc11c1e3
Zachary Rener <me@zacharyrener.com>
2020-11-08 14:46:53 -0600

    Added basic theme files

---
+++ footer.php
@@ -0,0 +1,9 @@
+        <footer id="site-footer" role="contentinfo" class="header-footer-group">
+
+		    <?php wp_footer(); ?>
+
+        </footer><!-- #site-footer -->
+
+	</body>
+	<script src="<?php echo get_stylesheet_directory_uri(); ?>/mifw/build/main.min.js"></script>
+</html>

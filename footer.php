798d9e1cb10e960b6892755dc6fba3163d70826b
Zachary Rener <me@zacharyrener.com>
2020-11-08 15:33:24 -0600

    Added footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
         <footer id="site-footer" role="contentinfo" class="header-footer-group">
 
+			<?php get_template_part('/mifw/library/Footer/footer'); ?>
 		    <?php wp_footer(); ?>
 
         </footer><!-- #site-footer -->
